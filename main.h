0ef46f124ae5d53c2ace6e66aabfad916ff7632d
marialegl <psico.marialegl@gmail.com>
2023-12-03 17:52:04 -0800

    Header file containing declarations for all functions used in the C - Printf

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Maria Alejandra Gonzalez - Luis Alfonso Diaz
+ * Desc: Header file containing declarations for all functions
+ *       used in the C - Printf.
+ */
+int _printf(const char *format, ...);
+
+#endif
