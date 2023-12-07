ce2f5a716029dbc4743c0ab8183a36ff0bdd7edc
Luamix <luizalfonso611@gmail.com>
2023-12-07 00:10:24 -0500

    Point 1 finish try

--- _printf.c
+++ _printf.c
@@ -11,8 +11,6 @@ int _printf(char *format, ...)
     char *buffer = malloc(2000);
     va_list list;
 
-    if (format == NULL || buffer == NULL)
-        return (-1);
     va_start(list, format);
     if (!correct_printf(format, buffer))
         exit(1);
