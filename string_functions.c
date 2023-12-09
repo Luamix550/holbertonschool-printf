c9da33b136083e15887965e5be5b74ff9a0b8310
Luamix <luizalfonso611@gmail.com>
2023-12-08 19:39:26 -0500

    other int_string_char

--- string_functions.c
+++ string_functions.c
@@ -1,5 +1,58 @@
 #include "main.h"
 
+/**
+ * print_chr - the function name
+ * @buffer: printf string content
+ * @counter: count arg
+ * @list: number for the list the arg the string
+ * Return: 0
+*/
+
+int print_chr(char *buffer, int counter, va_list list)
+{
+	buffer[0] = va_arg(list, int);
+	counter++;
+	return (counter);
+}
+/**
+ * print_str - print a string.
+ * @buffer: string buf.
+ * @counter: count string
+ * @list: va_list arg
+ * Return: long to print
+*/
+
+int print_str(char *buffer, int counter, va_list list)
+{
+	char *s;
+	int i = 0;
+
+	s = va_arg(list, char*);
+	if (s == NULL)
+		s = "(null)";
+	while (s[i] != '\0')
+	{
+		buffer[i] = s[i];
+		i++;
+		counter++;
+	}
+	return (counter);
+}
+/**
+* print_mod - print '%' character
+* @buffer: character buf
+* @counter: counter string
+* @list: va_list arguments
+* Return: long to string
+*/
+
+int print_mod(char *buffer, int counter, va_list __attribute__((unused)) list)
+{
+	buffer[0] = '%';
+	counter++;
+	return (counter);
+}
+
 int _strlen(char *str)
 {
     int i = 0;
